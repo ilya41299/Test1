433bacc267753dd5fc2e973d4b13817641c940dc
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2018-05-04 12:06:46 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -6,3 +6,14 @@ int median(int v1, int v2, int v3)
 	else if ((v1 <= v2 && v2 <= v3) || (v3 <= v2 && v2 <= v1)) return v2;
 	else return v3;
 }
+
+unsigned count_of_bits(unsigned value)
+{
+	unsigned int count = 0;
+	while (value!=1) 
+	{
+		if (value % 2) count++;
+		value = value / 2;
+	}
+	return ++count;
+}
